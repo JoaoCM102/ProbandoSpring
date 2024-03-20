44c34d5983ee5e65eb250f987da88d6bebc81995
Joao Pedro Lima Dias (ext) <joao.lima.external@eviden.com>
2024-03-20 14:17:46 +0000

    PadreListo

---
+++ src/main/java/com/prueba/stringProbando/Entidades/Familia/Padre.java
@@ -0,0 +1,11 @@
+package com.prueba.stringProbando.Entidades.Familia;
+
+public class Padre implements Familias {
+
+	@Override
+	public void datosFamilia(String miembro) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
